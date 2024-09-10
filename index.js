353c916f36e22aa5b5c806ac09273ade6c0dbe1a
Margarita827 <margaiba5@gmail.com>
2024-09-10 18:50:16 +0100

    prompt and answer

---
+++ index.js
@@ -0,0 +1,13 @@
+function showAnswer(response) {
+    
+}
+
+
+let apiKey = "o84c2a432ct7ef09733306be2507e42f";
+let context = "provide a short and concise answer"; 
+let prompt = "Who the first female president was?"
+
+let apiUrl =
+`https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+axios.get(apiUrl).then(showAnswer)
